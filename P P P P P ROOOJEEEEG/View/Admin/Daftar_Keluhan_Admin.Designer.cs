9706e8dfbe0c8bea18e55afc4225e78d1b6d4500
Atha Rifyan Adyfy <bismarponsel81@gmail.com>
2025-05-22 16:12:08 +0700

    update logo unej

--- P P P P P ROOOJEEEEG/View/Admin/Daftar_Keluhan_Admin.Designer.cs
+++ P P P P P ROOOJEEEEG/View/Admin/Daftar_Keluhan_Admin.Designer.cs
@@ -436,6 +436,7 @@
             label12.Size = new Size(125, 24);
             label12.TabIndex = 4;
             label12.Text = "Tinjau Ulasan";
+            label12.Click += ToTinjauUlasan_AD;
             // 
             // pictureBox1
             // 
@@ -446,6 +447,7 @@
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 2;
             pictureBox1.TabStop = false;
+            pictureBox1.Click += ToTinjauUlasan_AD;
             // 
             // pictureBox9
             // 
